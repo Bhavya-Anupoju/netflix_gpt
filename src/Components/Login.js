a4bb24fede73e3a88b5c35bc6ffff4f11180ca8a
Bhavya-Anupoju <bhavya.anupoju@gmail.com>
2023-10-29 11:43:57 +0530

    made responsive with Mobile

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -76,7 +76,7 @@ const Login = () =>{
         <div>
             <Header />
             <div className="absolute">    
-                <img className="h-screen object-cover" src={LOGO} alt="background" />
+                <img className="h-screen object-cover md:h-auto" src={LOGO} alt="background" />
             </div>
                 <form onSubmit={(e) => e.preventDefault()}className="bg-black p-12 absolute w-full md:w-3/12 my-24 m-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
                     <h1 className="font-bold text-2xl py-4">{isSignInForm ? "Sign In" : "Sign up"}</h1>
